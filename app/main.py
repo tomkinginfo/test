0edb9e3e8f43d0ebb3c1b7cf8001de20383595fc
tomkinginfo <115073455+tomkinginfo@users.noreply.github.com>
2022-11-24 15:41:59 +0800

    main.py
    test

--- app/main.py
+++ app/main.py
@@ -59,7 +59,7 @@ def validation_exception_handler(request, exc):
 # 基本的 get 用法
 @app.get("/", tags=['分類1'])
 def hello_world():
-    """這邊可以寫 API 的說明
+    """這邊可以寫 API 的說明aaaaaaaaa
     """
     return {"status": True}
 
